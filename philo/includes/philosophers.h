4db29e767abd6c5e65fa62eff6f3ec38b5f46bbf
Paul Louvel <plouvel@student.42.fr>
2022-04-18 11:55:09 +0200

    initial parsing and errors detection.

--- philo/includes/philosophers.h
+++ philo/includes/philosophers.h
@@ -6,26 +6,47 @@
 /*   By: plouvel <plouvel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 15:38:20 by plouvel           #+#    #+#             */
-/*   Updated: 2022/03/25 15:46:49 by plouvel          ###   ########.fr       */
+/*   Updated: 2022/04/18 11:53:25 by plouvel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 
+# include <stdbool.h>
+
+# define STR_PGR_NAME    "./philo"
 # define STR_P_TAKE_FORK "%lu %u has taken a fork\n"
 # define STR_P_EATING    "%lu %u is eating\n"
 # define STR_P_SLEEPING  "%lu %u is sleeping\n"
 # define STR_P_THINKING  "%lu %u is thinking\n"
 # define STR_P_DIED      "%lu %u died\n"
+# define STR_ERROR       "error: "
+# define STR_BAD_ARG     "bad argument (%u): "
+# define STR_NOT_DIGIT   STR_ERROR STR_BAD_ARG "not a valid positive unsigned integer.\n"
+# define STR_VAL_OVERF   STR_ERROR STR_BAD_ARG "value overflowing, max : \
+%u.\n"
+
+# define STR_USAGE        "usage : %s \
+<number_of_philosophers> (ms) <time_to_die> (ms) \
+<time_to_eat> (ms) <time_to_sleep> (ms) \
+[number_of_times_each_philosopher_must_eat]\n"
 
 typedef struct s_program
 {
+	char		*name;
 	unsigned int nbr_philo;
 	unsigned int nbr_philo_must_eat;
 	unsigned int time_to_die;
 	unsigned int time_to_eat;
 	unsigned int time_to_sleep;
 }				t_program;
+
+typedef struct s_philosopher
+{
+
+}				t_philosopher;
+
+bool	is_digit(int c);
 
 #endif
---
+++ philo/srcs/main.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plouvel <plouvel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/18 11:14:34 by plouvel           #+#    #+#             */
+/*   Updated: 2022/04/18 11:54:10 by plouvel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+
+unsigned int	*ft_ato_ui(const char *nptr, unsigned int *result)
+{
+	*result = 0;
+	while (*nptr != '\0')
+	{
+		*result = *result * 10 + (*nptr++ - '0');
+		if (*result > INT_MAX)
+			return (NULL);
+	}
+	return (result);
+}
+
+size_t	parse_arguments(t_program *program, int argc, char **argv)
+{
+	if (!ft_ato_ui(argv[1], &program->nbr_philo))
+		return (1);
+	if (!ft_ato_ui(argv[2], &program->time_to_die))
+		return (2);
+	if (!ft_ato_ui(argv[3], &program->time_to_eat))
+		return (3);
+	if (!ft_ato_ui(argv[4], &program->time_to_sleep))
+		return (4);
+	if (argc == 6)
+	{
+		if (!ft_ato_ui(argv[5], &program->nbr_philo_must_eat))
+			return (5);
+	}
+	else
+		program->nbr_philo_must_eat = 0;
+	return (0);
+}
+
+size_t	check_arguments(int argc, char **argv)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 1;
+	while (i < (size_t) argc)
+	{
+		j = 0;
+		if (argv[i][0] == '0' && argv[i][1] == '\0')
+			return (i);
+		while (argv[i][j] != '\0')
+		{
+			if (!is_digit(argv[i][j]))
+				return (i);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	setup_program(t_program *program, int argc, char **argv)
+{
+	size_t	arg;
+
+	if (argv[0] == NULL)
+		program->name = STR_PGR_NAME;
+	else
+		program->name = argv[0];
+	if (argc < 5 || argc > 6)
+		return (printf(STR_USAGE, program->name));
+	arg = check_arguments(argc, argv);
+	if (arg != 0)
+		return (printf(STR_NOT_DIGIT, (unsigned int) arg));
+	arg = parse_arguments(program, argc, argv);
+	if (arg != 0)
+		return (printf(STR_VAL_OVERF, (unsigned int) arg, INT_MAX));
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_program	program;
+
+	if (setup_program(&program, argc, argv) != 0)
+		return (1);
+	return (0);
+}
